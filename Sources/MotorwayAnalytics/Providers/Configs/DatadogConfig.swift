9d47eccd382ee1781b16d99df68797cbef0ef957
shabsmotorway <shabeer@motorway.co.uk>
2023-08-10 15:33:18 +0100

    DC-789 updated configs to use public init

--- Sources/MotorwayAnalytics/Providers/Configs/DatadogConfig.swift
+++ Sources/MotorwayAnalytics/Providers/Configs/DatadogConfig.swift
@@ -5,4 +5,11 @@ public struct DatadogConfig {
     let clientToken: String
     let environment: String
     let firstPartyHosts: Set<String>
+    
+    public init(rumApplicationID: String, clientToken: String, environment: String, firstPartyHosts: Set<String>) {
+        self.rumApplicationID = rumApplicationID
+        self.clientToken = clientToken
+        self.environment = environment
+        self.firstPartyHosts = firstPartyHosts
+    }
 }
--- Sources/MotorwayAnalytics/Providers/Configs/SnowplowConfig.swift
+++ Sources/MotorwayAnalytics/Providers/Configs/SnowplowConfig.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct SnowplowConfig {
     let appId: String
     let endpoint: String
+    
+    public init(appId: String, endpoint: String) {
+        self.appId = appId
+        self.endpoint = endpoint
+    }
 }
